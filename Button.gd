3b8694d45c5d1a68050493e8d94c1051d0f918af
Ciaran Manca <ciaranmanca@gmail.com>
2020-04-21 12:37:01 +0200

    prints poggers

--- Button.gd
+++ Button.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Button
 
 
 # Declare member variables here. Examples:
@@ -9,7 +9,6 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("poggers")
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
